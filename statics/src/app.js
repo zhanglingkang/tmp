b965929d5079458f8fdfa6d485e671deed30b19e
张凌康 <lingkang.zlk@alibaba-inc.com>
2015-12-13 22:39:46 +0800

    init

--- statics/src/app.js
+++ statics/src/app.js
@@ -5,4 +5,8 @@ var ReactDOM = require('react-dom')
 var {Router,Route,Link} = require('react-router')
 var $ = require('jquery')
 var routes = require('./routes')
-ReactDOM.render(<Router routes={routes}/>, $('#react-root')[0])
+var createHistory = require('history/lib/createHashHistory')
+var history = createHistory({
+    queryKey: false
+})
+ReactDOM.render(<Router routes={routes} history={history}/>, $('#react-root')[0])
